3e0520ef5335c65bd78bcd8c95ccb6481354a60d
jhy0409 <inooph@naver.com>
2021-03-09 15:08:18 +0900

    add

---
+++ 2 vsStudio_P/210309_cSharp_addressTest_P/addrWin0308/addrWin0309_2_1pm_org/UI/ViewForm.Designer.cs
@@ -0,0 +1,237 @@
+﻿
+namespace addrWin0302.UI
+{
+    partial class ViewForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.listView = new System.Windows.Forms.ListView();
+            this.list_count = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.list_name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.list_tel = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.list_addr = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.list_email = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.gridView = new Sunny.UI.UIDataGridView();
+            this.grid_count = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.grid_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.grid_tel = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.grid_addr = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.grid_email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.viewExit = new Sunny.UI.UISymbolButton();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listView
+            // 
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.list_count,
+            this.list_name,
+            this.list_tel,
+            this.list_addr,
+            this.list_email});
+            this.listView.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.listView.FullRowSelect = true;
+            this.listView.GridLines = true;
+            this.listView.HideSelection = false;
+            this.listView.Location = new System.Drawing.Point(35, 118);
+            this.listView.Margin = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(650, 180);
+            this.listView.TabIndex = 0;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            this.listView.SelectedIndexChanged += new System.EventHandler(this.listView_SelectedIndexChanged_1);
+            // 
+            // list_count
+            // 
+            this.list_count.Text = "번호";
+            this.list_count.Width = 45;
+            // 
+            // list_name
+            // 
+            this.list_name.Text = "이름";
+            this.list_name.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.list_name.Width = 70;
+            // 
+            // list_tel
+            // 
+            this.list_tel.Text = "전화";
+            this.list_tel.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.list_tel.Width = 140;
+            // 
+            // list_addr
+            // 
+            this.list_addr.Text = "주소";
+            this.list_addr.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.list_addr.Width = 200;
+            // 
+            // list_email
+            // 
+            this.list_email.Text = "이메일";
+            this.list_email.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.list_email.Width = 150;
+            // 
+            // gridView
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(243)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.gridView.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.gridView.BackgroundColor = System.Drawing.Color.White;
+            this.gridView.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(160)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("맑은 고딕", 9.75F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.gridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.gridView.ColumnHeadersHeight = 32;
+            this.gridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.gridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.grid_count,
+            this.grid_name,
+            this.grid_tel,
+            this.grid_addr,
+            this.grid_email});
+            this.gridView.EnableHeadersVisualStyles = false;
+            this.gridView.Font = new System.Drawing.Font("Microsoft YaHei", 12F);
+            this.gridView.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(160)))), ((int)(((byte)(255)))));
+            this.gridView.Location = new System.Drawing.Point(35, 313);
+            this.gridView.Margin = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.gridView.Name = "gridView";
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("맑은 고딕", 9.75F);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.gridView.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("맑은 고딕", 9.75F);
+            this.gridView.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            this.gridView.RowTemplate.Height = 29;
+            this.gridView.SelectedIndex = -1;
+            this.gridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.gridView.ShowGridLine = true;
+            this.gridView.Size = new System.Drawing.Size(650, 180);
+            this.gridView.TabIndex = 1;
+            // 
+            // grid_count
+            // 
+            this.grid_count.HeaderText = "번호";
+            this.grid_count.Name = "grid_count";
+            this.grid_count.Width = 50;
+            // 
+            // grid_name
+            // 
+            this.grid_name.HeaderText = "이름";
+            this.grid_name.Name = "grid_name";
+            this.grid_name.Width = 70;
+            // 
+            // grid_tel
+            // 
+            this.grid_tel.HeaderText = "전화";
+            this.grid_tel.Name = "grid_tel";
+            this.grid_tel.Width = 130;
+            // 
+            // grid_addr
+            // 
+            this.grid_addr.HeaderText = "주소";
+            this.grid_addr.Name = "grid_addr";
+            this.grid_addr.Width = 150;
+            // 
+            // grid_email
+            // 
+            this.grid_email.HeaderText = "이메일";
+            this.grid_email.Name = "grid_email";
+            this.grid_email.Width = 180;
+            // 
+            // viewExit
+            // 
+            this.viewExit.BackColor = System.Drawing.Color.Transparent;
+            this.viewExit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.viewExit.FillColor = System.Drawing.Color.Gray;
+            this.viewExit.FillHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(173)))), ((int)(((byte)(178)))), ((int)(((byte)(181)))));
+            this.viewExit.FillPressColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(103)))));
+            this.viewExit.FillSelectedColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(103)))));
+            this.viewExit.Font = new System.Drawing.Font("Microsoft YaHei", 12F);
+            this.viewExit.IsCircle = true;
+            this.viewExit.Location = new System.Drawing.Point(634, 36);
+            this.viewExit.MinimumSize = new System.Drawing.Size(1, 1);
+            this.viewExit.Name = "viewExit";
+            this.viewExit.RectColor = System.Drawing.Color.Gray;
+            this.viewExit.RectHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(173)))), ((int)(((byte)(178)))), ((int)(((byte)(181)))));
+            this.viewExit.RectPressColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(103)))));
+            this.viewExit.RectSelectedColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(103)))));
+            this.viewExit.Size = new System.Drawing.Size(50, 50);
+            this.viewExit.Style = Sunny.UI.UIStyle.Custom;
+            this.viewExit.Symbol = 61457;
+            this.viewExit.SymbolSize = 26;
+            this.viewExit.TabIndex = 14;
+            this.viewExit.Click += new System.EventHandler(this.viewExit_Click);
+            // 
+            // ViewForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(720, 530);
+            this.Controls.Add(this.gridView);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.viewExit);
+            this.Name = "ViewForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "주소록 데이터 보기";
+            this.Load += new System.EventHandler(this.ViewForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView;
+        private Sunny.UI.UIDataGridView gridView;
+        private Sunny.UI.UISymbolButton viewExit;
+        private System.Windows.Forms.ColumnHeader list_count;
+        private System.Windows.Forms.ColumnHeader list_name;
+        private System.Windows.Forms.ColumnHeader list_tel;
+        private System.Windows.Forms.ColumnHeader list_addr;
+        private System.Windows.Forms.ColumnHeader list_email;
+        private System.Windows.Forms.DataGridViewTextBoxColumn grid_count;
+        private System.Windows.Forms.DataGridViewTextBoxColumn grid_name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn grid_tel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn grid_addr;
+        private System.Windows.Forms.DataGridViewTextBoxColumn grid_email;
+    }
+}
