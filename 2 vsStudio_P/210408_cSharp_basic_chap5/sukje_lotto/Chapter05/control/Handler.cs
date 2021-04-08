25d7b732ad57b6ceb3d3ef9db80b3e5efae91507
jhy0409 <inooph@naver.com>
2021-04-09 00:01:03 +0900

    분리전2

--- 2 vsStudio_P/210408_cSharp_basic_chap5/sukje_lotto/Chapter05/control/Handler.cs
+++ 2 vsStudio_P/210408_cSharp_basic_chap5/sukje_lotto/Chapter05/control/Handler.cs
@@ -4,42 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Chapter05.control
+namespace Chapter05.cont
 {
     class Handler
     {
-        private List<LottoN> lottoNumNIndex;
-        double[] joncheHwaklyul = { 9.3, 8.7, 8.7, 9.3,  8.4,
-                                    8.4, 8.4, 8.4, 7,   9,
-                                    8.6, 9.2, 9.3, 8.7, 8.4,
-
-                                    8.4, 9.3, 8.9, 8.5, 9,
-                                    8.5, 7.1, 7.5, 8.6, 8,
-                                    8.8, 9.5, 7.7, 7.4, 8.3,
-
-                                    8.7, 7.7, 9.1, 9.4, 8.2,
-                                    8.4, 8.6, 8.9, 9.1, 8.9,
-                                    7.6, 8.3, 10,  8.3, 8.5};
-
-        public Handler()
-        {
-            List<LottoN> lottoNumNIndex = new List<LottoN>();
-            iplyok();
-        }
-
-        private void iplyok()
-        {
-            // 1~957회차 전체번호(1~45)별 확률 (로또 홈페이지 자료 참고)
-            for (int i = 0; i < 45; i++)
-            {
-                lottoNumNIndex.Add(new LottoN((i + 1), joncheHwaklyul[i]));
-            }
-        }
-
-        public List<LottoN> getList()
-        {
-            return lottoNumNIndex;
-        }
-
     }
 }
