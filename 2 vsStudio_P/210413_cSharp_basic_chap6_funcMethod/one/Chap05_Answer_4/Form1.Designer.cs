19f2ed14a097e59c48241795eb57352d5db368ed
jhy0409 <inooph@naver.com>
2021-04-14 21:01:05 +0900

    4/13 폴더 정리 및 추가

--- 2 vsStudio_P/210413_cSharp_basic_chap6_funcMethod/one/Chap05_Answer_4/Form1.Designer.cs
+++ 2 vsStudio_P/210413_cSharp_basic_chap6_funcMethod/one/Chap05_Answer_4/Form1.Designer.cs
@@ -39,7 +39,7 @@ namespace Chap05_Answer_4
             this.button7 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
+            this.ListText = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -50,8 +50,9 @@ namespace Chap05_Answer_4
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 30);
             this.button1.TabIndex = 0;
-            this.button1.Text = "12";
+            this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
@@ -79,8 +80,9 @@ namespace Chap05_Answer_4
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(100, 30);
             this.button2.TabIndex = 3;
-            this.button2.Text = "44";
+            this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -90,8 +92,9 @@ namespace Chap05_Answer_4
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(100, 30);
             this.button3.TabIndex = 4;
-            this.button3.Text = "27";
+            this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -101,8 +104,9 @@ namespace Chap05_Answer_4
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(100, 30);
             this.button4.TabIndex = 5;
-            this.button4.Text = "16";
+            this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button8
             // 
@@ -112,8 +116,9 @@ namespace Chap05_Answer_4
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(100, 30);
             this.button8.TabIndex = 9;
-            this.button8.Text = "16";
+            this.button8.Text = "button8";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button7
             // 
@@ -123,8 +128,9 @@ namespace Chap05_Answer_4
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(100, 30);
             this.button7.TabIndex = 8;
-            this.button7.Text = "27";
+            this.button7.Text = "button7";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button6
             // 
@@ -134,8 +140,9 @@ namespace Chap05_Answer_4
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(100, 30);
             this.button6.TabIndex = 7;
-            this.button6.Text = "44";
+            this.button6.Text = "button6";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button5
             // 
@@ -145,24 +152,25 @@ namespace Chap05_Answer_4
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(100, 30);
             this.button5.TabIndex = 6;
-            this.button5.Text = "12";
+            this.button5.Text = "button5";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
-            // label3
+            // ListText
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(273, 79);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(69, 12);
-            this.label3.TabIndex = 10;
-            this.label3.Text = "5단원 34/56";
+            this.ListText.AutoSize = true;
+            this.ListText.Location = new System.Drawing.Point(273, 79);
+            this.ListText.Name = "ListText";
+            this.ListText.Size = new System.Drawing.Size(69, 12);
+            this.ListText.TabIndex = 10;
+            this.ListText.Text = "5단원 34/56";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ListText);
             this.Controls.Add(this.button8);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
@@ -192,7 +200,7 @@ namespace Chap05_Answer_4
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label ListText;
     }
 }
 
--- 2 vsStudio_P/210413_cSharp_basic_chap6_funcMethod/one/Chap05_Answer_4/Form1.cs
+++ 2 vsStudio_P/210413_cSharp_basic_chap6_funcMethod/one/Chap05_Answer_4/Form1.cs
@@ -12,9 +12,103 @@ namespace Chap05_Answer_4
 {
     public partial class Form1 : Form
     {
+        public List<string> list = new List<string>();
         public Form1()
         {
             InitializeComponent();
+
+            Random rand = new Random();
+            button1.Text = rand.Next(100).ToString();
+            button5.Text = button1.Text;
+
+            button2.Text = rand.Next(100).ToString();
+            button6.Text = button2.Text;
+
+            button3.Text = rand.Next(100).ToString();
+            button7.Text = button3.Text;
+
+            button4.Text = rand.Next(100).ToString();
+            button8.Text = button4.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            list.Add(button1.Text);
+            ListText.Text = "";
+            foreach (var item in list)
+            {
+                ListText.Text += item + " ";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            list.Add(button2.Text);
+            ListText.Text = "";
+            foreach (var item in list)
+            {
+                ListText.Text += item + " ";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            list.Add(button3.Text);
+            ListText.Text = "";
+            foreach (var item in list)
+            {
+                ListText.Text += item + " ";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            list.Add(button4.Text);
+            ListText.Text = "";
+            foreach (var item in list)
+            {
+                ListText.Text += item + " ";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            list.Remove(button5.Text);
+            ListText.Text = "";
+            foreach (var item in list)
+            {
+                ListText.Text += item + " ";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            list.Remove(button6.Text);
+            ListText.Text = "";
+            foreach (var item in list)
+            {
+                ListText.Text += item + " ";
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            list.Remove(button7.Text);
+            ListText.Text = "";
+            foreach (var item in list)
+            {
+                ListText.Text += item + " ";
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            list.Remove(button8.Text);
+            ListText.Text = "";
+            foreach (var item in list)
+            {
+                ListText.Text += item + " ";
+            }
         }
     }
 }
