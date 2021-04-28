17f4cc4b4d32955c09c72a712fbd40affc7bbbe5
jhy0409 <inooph@naver.com>
2021-04-28 12:23:22 +0900

    파일 정리 중

---
+++ 5 Python/210412_varNameAutoCreate_mine/WindowsFormsApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("1. 변수이름 입력 : ");
+            string mkByonsu = Console.ReadLine();
+            
+            Console.Write("2. 변수생성 개수 : ");
+            int makeHow = int.Parse(Console.ReadLine());
+            string sum = "";
+            string temp = "";
+            for (int i = 0; i < makeHow; i++)
+            {
+                if (i == makeHow-1)
+                {
+                    temp = mkByonsu + (i + 1).ToString();
+                    sum += temp;
+                }
+                else
+                {
+                    temp = mkByonsu + (i + 1).ToString() + ", ";
+                    sum += temp;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
