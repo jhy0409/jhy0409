45c1b93309e57e153f36cebcc01ddf407715d45e
jhy0409 <inooph@naver.com>
2021-05-07 10:20:09 +0900

    일부수정

--- 0 portfolio/210504_이력서+ppt/프로젝트+ppt/3_1_[팀프로젝트]_약국재고관리 프로그램_210421/1teamProject/teamProject/UI/MainDrugs.Designer.cs
+++ 0 portfolio/210504_이력서+ppt/프로젝트+ppt/3_1_[팀프로젝트]_약국재고관리 프로그램_210421/1teamProject/teamProject/UI/MainDrugs.Designer.cs
@@ -39,13 +39,14 @@ namespace teamProject
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.drugBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.medNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.medCompDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.medIdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -54,7 +55,6 @@ namespace teamProject
             this.classNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.mainIngrDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.stockDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.drugBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -64,9 +64,9 @@ namespace teamProject
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(151, 42);
+            this.textBox1.Location = new System.Drawing.Point(155, 42);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(216, 27);
+            this.textBox1.Size = new System.Drawing.Size(180, 27);
             this.textBox1.TabIndex = 2;
             // 
             // groupBox1
@@ -74,9 +74,9 @@ namespace teamProject
             this.groupBox1.Controls.Add(this.comboBox1);
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.textBox1);
-            this.groupBox1.Location = new System.Drawing.Point(13, 15);
+            this.groupBox1.Location = new System.Drawing.Point(44, 15);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(482, 100);
+            this.groupBox1.Size = new System.Drawing.Size(435, 100);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "약품 정보 조회";
@@ -89,40 +89,40 @@ namespace teamProject
             "품목명",
             "제조회사",
             "품목번호"});
-            this.comboBox1.Location = new System.Drawing.Point(22, 41);
+            this.comboBox1.Location = new System.Drawing.Point(26, 41);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 28);
             this.comboBox1.TabIndex = 6;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(374, 41);
+            this.button1.Location = new System.Drawing.Point(341, 41);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(89, 29);
+            this.button1.Size = new System.Drawing.Size(70, 29);
             this.button1.TabIndex = 4;
-            this.button1.Text = "검색하기";
+            this.button1.Text = "검색";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // checkBox2
             // 
             this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(21, 65);
+            this.checkBox2.Location = new System.Drawing.Point(28, 65);
             this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(103, 24);
+            this.checkBox2.Size = new System.Drawing.Size(88, 24);
             this.checkBox2.TabIndex = 7;
-            this.checkBox2.Text = "재고적은순";
+            this.checkBox2.Text = "오름차순";
             this.checkBox2.UseVisualStyleBackColor = true;
             this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(21, 29);
+            this.checkBox1.Location = new System.Drawing.Point(28, 29);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(103, 24);
+            this.checkBox1.Size = new System.Drawing.Size(88, 24);
             this.checkBox1.TabIndex = 6;
-            this.checkBox1.Text = "재고많은순";
+            this.checkBox1.Text = "내림차순";
             this.checkBox1.UseVisualStyleBackColor = true;
             this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
@@ -130,12 +130,13 @@ namespace teamProject
             // 
             this.groupBox2.Controls.Add(this.checkBox1);
             this.groupBox2.Controls.Add(this.checkBox2);
-            this.groupBox2.Location = new System.Drawing.Point(501, 15);
+            this.groupBox2.Location = new System.Drawing.Point(486, 15);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 3, 3, 3);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(138, 100);
             this.groupBox2.TabIndex = 8;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "정렬하기";
+            this.groupBox2.Text = "재고정렬";
             // 
             // groupBox3
             // 
@@ -145,18 +146,35 @@ namespace teamProject
             this.groupBox3.Controls.Add(this.button2);
             this.groupBox3.Controls.Add(this.textBox2);
             this.groupBox3.Controls.Add(this.label2);
-            this.groupBox3.Location = new System.Drawing.Point(645, 15);
+            this.groupBox3.Location = new System.Drawing.Point(631, 15);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 3, 3, 3);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(351, 100);
+            this.groupBox3.Size = new System.Drawing.Size(333, 100);
             this.groupBox3.TabIndex = 9;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "약품 주문";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "약품명";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(93, 62);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(150, 27);
+            this.textBox3.TabIndex = 3;
+            // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(242, 25);
+            this.button3.Location = new System.Drawing.Point(248, 25);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(98, 29);
+            this.button3.Size = new System.Drawing.Size(70, 29);
             this.button3.TabIndex = 4;
             this.button3.Text = "담기";
             this.button3.UseVisualStyleBackColor = true;
@@ -164,24 +182,17 @@ namespace teamProject
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(242, 61);
+            this.button2.Location = new System.Drawing.Point(248, 61);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(98, 29);
+            this.button2.Size = new System.Drawing.Size(70, 29);
             this.button2.TabIndex = 4;
-            this.button2.Text = "주문하기";
+            this.button2.Text = "주문";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(87, 62);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(150, 27);
-            this.textBox3.TabIndex = 3;
-            // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(87, 26);
+            this.textBox2.Location = new System.Drawing.Point(93, 26);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(150, 27);
             this.textBox2.TabIndex = 2;
@@ -189,20 +200,11 @@ namespace teamProject
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(8, 65);
+            this.label2.Location = new System.Drawing.Point(14, 65);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(74, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "주문 수량";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(8, 29);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(54, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "약품명";
             // 
             // dataGridView1
             // 
@@ -219,14 +221,18 @@ namespace teamProject
             this.mainIngrDataGridViewTextBoxColumn,
             this.stockDataGridViewTextBoxColumn});
             this.dataGridView1.DataSource = this.drugBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(13, 121);
+            this.dataGridView1.Location = new System.Drawing.Point(44, 135);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowTemplate.Height = 23;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(983, 590);
+            this.dataGridView1.Size = new System.Drawing.Size(920, 550);
             this.dataGridView1.TabIndex = 10;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // drugBindingSource
+            // 
+            this.drugBindingSource.DataSource = typeof(teamProject.Drug);
             // 
             // medNameDataGridViewTextBoxColumn
             // 
@@ -267,9 +273,10 @@ namespace teamProject
             // classNameDataGridViewTextBoxColumn
             // 
             this.classNameDataGridViewTextBoxColumn.DataPropertyName = "ClassName";
-            this.classNameDataGridViewTextBoxColumn.HeaderText = "약효분류";
+            this.classNameDataGridViewTextBoxColumn.HeaderText = "약효";
             this.classNameDataGridViewTextBoxColumn.Name = "classNameDataGridViewTextBoxColumn";
             this.classNameDataGridViewTextBoxColumn.ReadOnly = true;
+            this.classNameDataGridViewTextBoxColumn.Width = 70;
             // 
             // mainIngrDataGridViewTextBoxColumn
             // 
@@ -282,14 +289,10 @@ namespace teamProject
             // stockDataGridViewTextBoxColumn
             // 
             this.stockDataGridViewTextBoxColumn.DataPropertyName = "Stock";
-            this.stockDataGridViewTextBoxColumn.HeaderText = "재고수량";
+            this.stockDataGridViewTextBoxColumn.HeaderText = "재고";
             this.stockDataGridViewTextBoxColumn.Name = "stockDataGridViewTextBoxColumn";
             this.stockDataGridViewTextBoxColumn.ReadOnly = true;
-            this.stockDataGridViewTextBoxColumn.Width = 95;
-            // 
-            // drugBindingSource
-            // 
-            this.drugBindingSource.DataSource = typeof(teamProject.Drug);
+            this.stockDataGridViewTextBoxColumn.Width = 70;
             // 
             // MainDrugs
             // 
@@ -337,6 +340,7 @@ namespace teamProject
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.BindingSource drugBindingSource;
+        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.DataGridViewTextBoxColumn medNameDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn medCompDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn medIdDataGridViewTextBoxColumn;
@@ -345,6 +349,5 @@ namespace teamProject
         private System.Windows.Forms.DataGridViewTextBoxColumn classNameDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn mainIngrDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn stockDataGridViewTextBoxColumn;
-        private System.Windows.Forms.Button button3;
     }
 }
